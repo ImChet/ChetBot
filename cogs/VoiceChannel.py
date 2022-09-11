b7c8e07e8d7c0931c2a7f33e4d9e08542b2c4b99
Andrew Martin <imchet.w@gmail.com>
2022-09-11 16:59:14 -0400

    Removed pass_context, deprecated

--- cogs/VoiceChannel.py
+++ cogs/VoiceChannel.py
@@ -10,7 +10,7 @@ class VoiceChannel(commands.Cog, name='Voice Channel Commands'):
         self.ChetBot = ChetBot
 
     # Join voice channel
-    @commands.command(pass_context=True)
+    @commands.command()
     async def join(self, ctx):
         if ctx.author.voice:
             await ctx.message.author.voice.channel.connect()
@@ -18,7 +18,7 @@ class VoiceChannel(commands.Cog, name='Voice Channel Commands'):
             await ctx.send(f'{ctx.author.mention}, you are not in a voice channel.', delete_after=5)
 
     # Leave voice channel
-    @commands.command(pass_context=True)
+    @commands.command()
     async def leave(self, ctx):
         if ctx.voice_client:
             await ctx.guild.voice_client.disconnect()
@@ -26,7 +26,7 @@ class VoiceChannel(commands.Cog, name='Voice Channel Commands'):
             await ctx.send(f'{ctx.author.mention}, I am not in a voice channel.', delete_after=5)
 
     # Play file located at arg
-    @commands.command(pass_context=True)
+    @commands.command()
     async def play(self, ctx, arg):
         if ctx.guild.voice_client is not None:
             voice = ctx.guild.voice_client
@@ -36,7 +36,7 @@ class VoiceChannel(commands.Cog, name='Voice Channel Commands'):
             await ctx.send(f'{ctx.author.mention}, I am not in a voice channel.', delete_after=5)
 
     # Queues file located at arg to play next
-    @commands.command(pass_context=True)
+    @commands.command()
     async def queue(self, ctx, arg):
         source = FFmpegPCMAudio(arg)
         guild_id = ctx.message.guild.id
@@ -47,7 +47,7 @@ class VoiceChannel(commands.Cog, name='Voice Channel Commands'):
         await ctx.send(f'{ctx.author.mention}, your request is added to the queue.', delete_after=5)
 
     # Pause file currently being played
-    @commands.command(pass_context=True)
+    @commands.command()
     async def pause(self, ctx):
         voice = discord.utils.get(self.ChetBot.voice_clients, guild=ctx.guild)
         if voice.is_playing():
@@ -56,7 +56,7 @@ class VoiceChannel(commands.Cog, name='Voice Channel Commands'):
             await ctx.send(f'{ctx.author.mention}, there is currently no audio being played.', delete_after=5)
 
     # Resume file currently paused
-    @commands.command(pass_context=True)
+    @commands.command()
     async def resume(self, ctx):
         voice = discord.utils.get(self.ChetBot.voice_clients, guild=ctx.guild)
         if voice.is_paused():
@@ -65,7 +65,7 @@ class VoiceChannel(commands.Cog, name='Voice Channel Commands'):
             await ctx.send(f'{ctx.author.mention}, there is currently no audio paused.', delete_after=5)
 
     # Stop file that is being played
-    @commands.command(pass_context=True)
+    @commands.command()
     async def stop(self, ctx):
         voice = discord.utils.get(self.ChetBot.voice_clients, guild=ctx.guild)
         voice.stop()
