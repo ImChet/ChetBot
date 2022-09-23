e74201876a57686d17bc497b185078427149d48b
Andrew Martin <imchet.w@gmail.com>
2022-09-22 20:51:14 -0400

    Updated

--- cogs/OnReady.py
+++ cogs/OnReady.py
@@ -11,7 +11,7 @@ class OnReady(commands.Cog):
     # on_ready is called when the Bot has logged on/set things up
     @commands.Cog.listener()
     async def on_ready(self):
-        print(f'We have logged in as {self.ChetBot.user} on {getCurrentDateTime()}')
+        print(f'We have successfully logged in as {self.ChetBot.user} on {getCurrentDateTime()}.')
 
 
 async def setup(ChetBot):
