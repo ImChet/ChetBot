f27fd9c3308f25647fd4503b7571221d34b9c7c6
Andrew Martin <imchet.w@gmail.com>
2022-09-23 17:12:09 -0400

    Added feedback modal

---
+++ cogs/Modals.py
@@ -0,0 +1,38 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+
+# Modals can only have text
+class Feedback(discord.ui.Modal, title='Feedback on ChetBot'):
+    name = discord.ui.TextInput(
+        label='Name',
+        placeholder='Enter your name here...'
+    )
+    feedback = discord.ui.TextInput(
+        label='What feedback do you have?',
+        style=discord.TextStyle.paragraph,
+        placeholder='Type your feedback here...',
+        required=True,
+        max_length=500
+    )
+
+    async def on_submit(self, interaction: discord.Interaction) -> None:
+        # Currently the feedback doesn't get sent anywhere
+        await interaction.response.send_message(f'Thank you for submitting your feedback, {interaction.user.mention}!',
+                                                ephemeral=True)
+
+
+class Modals(commands.Cog):
+
+    def __init__(self, ChetBot):
+        self.ChetBot = ChetBot
+
+    @app_commands.command(name='feedback', description='Submit feedback to the creator of ChetBot.')
+    @app_commands.guilds(495623660967690240)
+    async def _feedback_modal_(self, interaction: discord.Interaction):
+        await interaction.response.send_modal(Feedback())
+
+
+async def setup(ChetBot):
+    await ChetBot.add_cog(Modals(ChetBot))
